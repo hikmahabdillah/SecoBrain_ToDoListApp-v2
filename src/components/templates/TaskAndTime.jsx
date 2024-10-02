8b2c14eb867cac7509c4d6ba1c93bf8b9037e9a8
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-10-02 21:39:39 +0700

    add event on completed n incompleted task

--- src/components/templates/TaskAndTime.jsx
+++ src/components/templates/TaskAndTime.jsx
@@ -2,7 +2,7 @@ import TaskCard from "../molecules/TaskCard";
 
 const TaskAndTime = ({isToday}) => {
   return (
-    <li className="mb-5 ms-4 text-left">
+    <li className={`mb-5 ${isToday && "ms-4"} text-left`}>
     <div className="flex items-center gap-2">
     <div className={`${isToday ? "absolute bg-[#0A26AE] -start-1.5" : "bg-gray-500"} w-3 h-3 rounded-full border border-white`} />
     <span className={`text-sm font-semibold font-poppins leading-none ${isToday ? "text-[#0A26AE]" : "text-gray-500" }`}>
--- src/components/templates/TaskHeader.jsx
+++ src/components/templates/TaskHeader.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import HeadPerSection from "../molecules/HeadPerSection";
 import TaskBadge from "../molecules/TaskBadge";
 
 const TaskHeader = () => {
+  const [isCompletedTab, setIsCompletedTab] = useState(false); // Rename state for clarity
+
+  const toggleTabs = () => {
+    setIsCompletedTab((prevTab) => !prevTab);
+  };
+
   return (
     <div className="taskHeader space-y-2">
+      {/* Header with tasks summary */}
       <div className="flex justify-between items-center gap-4">
         <HeadPerSection>You Have 7 tasks for today</HeadPerSection>
         <TaskBadge
@@ -12,15 +20,35 @@ const TaskHeader = () => {
           width="40px"
         />
       </div>
+
+      {/* Tasks status and tab switching */}
       <div className="flex justify-between items-center gap-4">
         <TaskBadge
           iconName={"../../taskInCompleted.svg"}
           width="25px"
           numOfTasks={3}
         />
-        <div className="flex items-center gap-4 [&>span]:duration-300 [&>span]:cursor-pointer">
-          <span className="active-tabs">Incompleted</span>
-          <span className="deactive-tabs">Completed</span>
+        <div className="flex items-center gap-4 cursor-pointer">
+          <button
+            type = "button"
+            className={`duration-300 ${
+              isCompletedTab ? "deactive-tabs" : "active-tabs"
+            }`}
+            onClick={toggleTabs}
+            aria-pressed={!isCompletedTab} // Aksesibilitas untuk menunjukkan status tab
+          >
+            Incompleted
+          </button>
+          <button
+            type = "button"
+            className={`duration-300 ${
+              isCompletedTab ? "active-tabs" : "deactive-tabs"
+            }`}
+            onClick={toggleTabs}
+            aria-pressed={isCompletedTab} // Aksesibilitas untuk menunjukkan status tab
+          >
+            Completed
+          </button>
         </div>
       </div>
     </div>
